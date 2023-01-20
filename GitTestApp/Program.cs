4d549aa4156c784be5900051ef3ef982f75b83d2
Редисочка <leon.d.dudkin@gmail.com>
2023-01-20 12:07:26 +0300

    Addition and substraction

---
+++ GitTestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+
+            result= a + b;
+            Console.WriteLine("{0} + {1} = {2}", a, b, result);
+
+            result = a - b;
+            Console.WriteLine("{0} - {1} = {2}", a, b, result);
+
+            Console.Read();
+        }
+    }
+}
